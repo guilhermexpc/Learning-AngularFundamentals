dd258aaba25f048f52ba17af029ad337166ac09b
Guilherme Campos <guilherme.campos@maxiconsystems.com.br>
2021-12-07 11:02:33 -0300

    Interpolation and Property  Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent implements OnInit {
+  url: string = 'http:loiane.com';
+  urlLoremImagem: string = 'https://picsum.photos/400/300';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getValue(value: number) {
+    return value;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
