33fe8c2d2dbcd510a0731deca0a0ccec20b6d333
Guilherme Campos <guilherme.campos@maxiconsystems.com.br>
2021-12-27 14:32:15 -0300

    diretivas renderer  e elementRef

--- diretivas-md04/src/app/app.module.ts
+++ diretivas-md04/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNGifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { FundoAmareloDirective } from './share/fundo-amarelo.directive';
 
 @NgModule({
-  declarations: [AppComponent, DiretivaNGifComponent],
+  declarations: [AppComponent, DiretivaNGifComponent, FundoAmareloDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
