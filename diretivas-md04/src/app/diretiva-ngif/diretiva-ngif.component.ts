dcb1d074a3740017346d402df476773b91cc32ad
Guilherme Campos <guilherme.campos@maxiconsystems.com.br>
2021-12-27 09:23:31 -0300

    ngif finalizado

--- diretivas-md04/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas-md04/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivaNGifComponent implements OnInit {
   cursos: string[] = ['Angular2'];
 
+  mostrarCursos: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
