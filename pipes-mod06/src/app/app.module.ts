7c3d10010596bd6f969f81e5072582a78cc2b935
Guilherme Campos <guilherme.campos@maxiconsystems.com.br>
2021-12-28 14:13:58 -0300

    com usar pipes

--- pipes-mod06/src/app/app.module.ts
+++ pipes-mod06/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExemplosComponent } from './exemplos/exemplos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosComponent
   ],
   imports: [
     BrowserModule,
---
+++ pipes-mod06/src/app/exemplos/exemplos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos',
+  templateUrl: './exemplos.component.html',
+  styleUrls: ['./exemplos.component.css'],
+})
+export class ExemplosComponent implements OnInit {
+  livro: any = {
+    titulo: 'Learning JavaScript Data Structures and Algorithms 2nd ed',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glqjpRP',
+  };
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
