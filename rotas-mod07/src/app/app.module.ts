571dc016e85bd4e98d5dc70294abcc5972fbe41b
Guilherme Campos <guilherme.campos@maxiconsystems.com.br>
2021-12-29 10:11:32 -0300

    reset rotas-mod07

--- rotas-mod07/src/app/app.module.ts
+++ rotas-mod07/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
